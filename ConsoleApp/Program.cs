7e71d924b2fac4127d2a8dd9d050fa653210a2af
Oomusou <oomusou@gmail.com>
2018-03-21 21:19:12 +0800

    ClassLibrary1

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var object1 = new Class1();
+            var result = object1.Sum(1, 1);
+            Console.WriteLine(result);
+        }
+    }
+}
